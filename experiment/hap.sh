20509f2d2446921634248f55d4246dd42af62906
yanxiaoliang <yanxiaoliang@gmail.com>
2014-05-07 12:14:01 -0400

    experiment scripts

---
+++ experiment/hap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#./run-hap
+#cd pjbb2005/
+#./run-hap.sh
+./run-hap-antlr
+./run-hap-bloat
+./run-hap-hsqldb
---
+++ experiment/shadow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#./run-shadow
+#cd pjbb2005/
+#./run-shadow.sh
+./run-shadow-antlr
+./run-shadow-bloat
+./run-shadow-hsqldb
